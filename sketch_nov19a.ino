3fb6e7eea259c5f5d715c57dc79658defdc7822e
Rewuugaboo42 <rewuugaboo42@gmail.com>
2024-11-19 11:08:24 +0100

    added specifications and comments

--- sketch_nov19a.ino
+++ sketch_nov19a.ino
@@ -1,24 +1,24 @@
-int numbers[10]{};
+constexpr int arrLen{ 10 };
+int numbers[arrLen]{};
 int sum{};
 
 constexpr int lightPin{ A0 };
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(lightPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for (int i{ 0 }; i < 10; ++i)
+  // Weird function using arrays following the specified specifications
+  for (int i{ 0 }; i < arrLen; ++i)
   {
     int arrayValue{ analogRead(lightPin) };
     numbers[i] = arrayValue;
     sum += numbers[i];
     numbers[i] = 0;
   }
-  int average = sum / 10;
+  int average = sum / arrLen;
   Serial.println(average);
   sum = 0;
   delay(500);
